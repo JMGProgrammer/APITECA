d9426efb728b28acc7d2bf4d61b145659c2cdc1c
Juan Meny <jemgprog@gmail.com>
2026-06-16 20:51:05 -0300

    v1.0

--- Program.cs
+++ Program.cs
@@ -15,12 +15,6 @@ builder.Services.AddDbContext<AppDbContext>(options => options
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-}
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
